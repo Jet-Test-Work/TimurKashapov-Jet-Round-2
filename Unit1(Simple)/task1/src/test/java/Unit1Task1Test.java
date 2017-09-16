0b53fd2b4ac9aa6008960b8c11250e20ec448661
Timur @shako Kashapov <timshako@gmail.com>
2017-09-16 16:33:30 +0300

    upd

---
+++ Unit1(Simple)/task1/src/test/java/Unit1Task1Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Unit1Task1Test {
+
+    @Test
+    public void getPrevBigElemOfInt() throws Exception {
+
+        // TO-DO
+
+    }
+}
