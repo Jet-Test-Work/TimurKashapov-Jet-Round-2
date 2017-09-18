331c161c037d7efb88f499586a805ceb1e66295f
Timur @shako Kashapov <timshako@gmail.com>
2017-09-19 00:58:07 +0300

    Добавил проверочные условия и рефактор

--- Unit1(Simple)/task2/src/main/java/Unit1Task2.java
+++ Unit1(Simple)/task2/src/main/java/Unit1Task2.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Программа выполняет разложение числа на набор простых множителей.
+ * Программа выполняет разложение числа на набор простых множителей (Факторизация).
  *
  * @author Timur Kashapov.
  * @since 0.0.1
@@ -15,7 +16,7 @@ public class Unit1Task2 {
      * @param str строка для вывода.
      */
     private static void println(String str) {
-        System.out.println(str);
+        System.out.println("\n" + str);
     } // println()
 
     /**
@@ -48,27 +49,31 @@ public class Unit1Task2 {
      * @param str исходная строка.
      * @return подтверждение.
      */
-    private static boolean isNumber(String str) {
+    protected static boolean isNumber(String str) {
 
         boolean ok = true;
 
-        char ch;
-        int  len = str.length();
+        if (str != null) {
+            if (str.length() > 0) {
+                char ch;
+                int  len = str.length();
+                // По символам элемента
+                for (int i = 0; i < str.length(); ++i) {
 
-        // По символам элемента
-        for (int i = 0; i < str.length(); ++i) {
+                    // Текущий символ строки.
+                    ch = str.charAt(i);
+                    // Если
+                    // 1. длина строки больше одного символа И
+                    // 2. проверяется первый символ И
+                    // 3. следующие за первым символом - цифра (0 <= ch <= 9)
+                    // - то этот символ интерпретируем как числовой знак и пропускаем итерацию.
+                    if (len > 1  && i == 0 && ch == '-' && str.charAt(i + 1) >= '0' && str.charAt(i + 1) <= '9') continue;
+                        // иначе проверяем символ на символ цифры.
+                    else { if ( ! (ch >= '0' && ch <= '9') ) { ok = false; break; } }
+                }
+            } else { ok = false; }
+        } else { ok = false; }
 
-            // Текущий символ строки.
-            ch = str.charAt(i);
-            // Если
-            // 1. длина строки больше одного символа И
-            // 2. проверяется первый символ И
-            // 3. следующие за первым символом - цифра (0 <= ch <= 9)
-            // - то этот символ интерпретируем как числовой знак и пропускаем итерацию.
-            if (len > 1  && i == 0 && ch == '-' && str.charAt(i + 1) >= '0' && str.charAt(i + 1) <= '9') continue;
-                // иначе проверяем символ на символ цифры.
-            else { if ( ! (ch >= '0' && ch <= '9') ) { ok = false; break; } }
-        }
         return ok;
 
     } // isNumber()
@@ -89,20 +94,66 @@ public class Unit1Task2 {
     } // toInteger()
 
     /**
+     * Является ли число простым.
+     *
+     * @return подтверждение.
+     */
+    protected static boolean isPrime(Long n) {
+
+        boolean result = false;
+
+        if ( n != null) {
+
+            if ( n < 2L ) { return false; }
+            else if ( n == 2L ) { return true; }
+            else if( n % 2L == 0L ) { return false; }
+
+            // пробный делитель
+            int d = 3;
+
+            // Алгоритм поиска простого числа.
+            // n делим по модулю на пробный делитель.
+            //
+            while( d*d <= n ) { if (n % d == 0) { return false; } d += 2; } result = true;
+        } else { result = false; }
+
+        return result;
+    } // isPrime()
+
+    /**
      * Разложить число на простые множители.
      *
-     * @param numb исходное число.
+     * @param n исходное число.
      * @return набор простых множителей.
      */
-    private static Long[] decomposeTheNumber(Long numb) {
+    private static Long[] decomposeTheNumber(Long n) {
 
-        //
-        //
-        //
-        //
-        println("DONE!");
+        ArrayList<Long> temp = new ArrayList<>();
+        int counter = 0;
 
-        return new Long[0];
+        // n - число для факторизации.
+        // d - делитель
+        // a - первый множитель
+        // b - второй множитель
+        Long a = n, b, d = 2L;
+
+        // Если исходное число не простое - раскладываем на множители
+        if ( ! isPrime(n) ) {
+            for(int i = 0; d <= n; ++i) {
+                if (a % d == 0L) {
+                    a /= d; b = d; // Сохраняем частное от деления и записываем частное в делимое.
+                    temp.add(b); ++counter; // записываем в массив и повышаем счетчик на 1
+                }
+                else { --i; if (d == 2L) { ++d; } else { d += 2L; } } }
+        }
+        // Иначе возвращаем исходное.
+        else { temp.add(n) ; }
+
+        Long[] result = new Long[temp.size()];
+
+        for (int i = 0; i < result.length; i++) { result[i] = temp.get(i); } // for i
+
+        return result;
     } // decomposeTheNumber()
 
     /**
@@ -120,11 +171,11 @@ public class Unit1Task2 {
 
         // Ввод аргументами через системную консоль.
         if (args.length > 0) {
-            if ( isNumber(args[0])) { Arrays.toString(getPrimes(args[0])); }
+            if ( isNumber(args[0])) { println(Arrays.toString(getPrimes(args[0]))); }
             else println("Incorrect input!");
         }
         // Интерактивный ввод через системную консоль.
-        else { Arrays.toString(getPrimes(getInput())); }
+        else { println(Arrays.toString(getPrimes(getInput()))); }
 
         println("\n");
     } // main()
