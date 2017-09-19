e4dd392d2f8fb03d9c72a3f34898c237b69d5d1e
Timur @shako Kashapov <timshako@gmail.com>
2017-09-19 13:57:50 +0300

    upd

--- Unit1(Simple)/task5/src/main/java/Unit1Task5.java
+++ Unit1(Simple)/task5/src/main/java/Unit1Task5.java
@@ -32,7 +32,7 @@ public class Unit1Task5 {
      * Поиск подстроки в строке.
      *
      * @param str строка.
-     * @param substr подстрока.
+     * @param subStr подстрока.
      * @return подтвержение.
      */
     private static boolean searchSubstring(String str, String subStr) {
@@ -41,11 +41,6 @@ public class Unit1Task5 {
 
         char[] charsOfStr    = toCharacterArray(str);
         char[] charsOfSubStr = toCharacterArray(subStr);
-
-
-
-
-
 
         return result;
     } // searchSubstring()
