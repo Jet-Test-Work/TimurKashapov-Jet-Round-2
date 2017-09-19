7e158b60368e74ece245e19e0bfebe89acdb6dbe
Timur @shako Kashapov <timshako@gmail.com>
2017-09-19 13:55:45 +0300

    Тестирование метода toCharacterArray()

--- Unit1(Simple)/task5/src/test/java/Unit1Task5Test.java
+++ Unit1(Simple)/task5/src/test/java/Unit1Task5Test.java
@@ -1,8 +1,25 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class Unit1Task5Test {
+    
     @Test
     public void toCharacterArray() throws Exception {
-    }
 
+        // Тестирование преобразования строки в массив символов.
+        //
+        char[] testCha00 = new char[]{};
+        String testStr00 = "";
+
+        char[] testCha01 = new char[]{'s'};
+        String testStr01 = "s";
+
+        char[] testCha02 = new char[]{'s','2','1','-','=','+',};
+        String testStr03 = "s21-=+";
+
+        assertArrayEquals( testCha00, Unit1Task5.toCharacterArray(testStr00) );
+        assertArrayEquals( testCha01, Unit1Task5.toCharacterArray(testStr01) );
+        assertArrayEquals( testCha02, Unit1Task5.toCharacterArray(testStr03) );
+    }
 }
